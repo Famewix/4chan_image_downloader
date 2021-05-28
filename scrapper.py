4cc799ef00dcb7c42f3508e47a6f05ae65fc788c
Faridin Islam <73052959+Famewix@users.noreply.github.com>
2021-05-28 17:02:13 +0600

    prints number of files downloaded at the end.

--- scrapper.py
+++ scrapper.py
@@ -7,11 +7,14 @@ import os, sys
 
 URL = input("4chan thread URL: ")
 folder_name = input("Folder to save images: ")
+count = 0
 
 def download_image(image_url,  name):
+    global count
     try:
         urlretrieve(image_url, name)
         print(f"{image_url} is downloaded")
+        count += 1
     except HTTPError:
         print(f"{image_url} returned HTTP error.")
 
@@ -42,3 +45,5 @@ def main(url, foldername):
 
 if __name__ == '__main__':
     main(URL, folder_name)
+    print('')
+    print(f"Downloaded {count} files.")
