caa1160f5a82096fef8a4689686168aa7ede871b
Faridin Islam <73052959+Famewix@users.noreply.github.com>
2021-06-09 20:01:05 +0600

    added colorama

--- scrapper.py
+++ scrapper.py
@@ -4,19 +4,22 @@ import requests
 from requests.exceptions import MissingSchema, InvalidURL
 from bs4 import BeautifulSoup
 import os, sys
+from colorama import Fore, init
+init(autoreset=True)
 
 URL = input("4chan thread URL: ")
 folder_name = input("Folder to save images: ")
 count = 0
 
+
 def download_image(image_url,  name):
     global count
     try:
         urlretrieve(image_url, name)
-        print(f"{image_url} is downloaded")
+        print(f"{Fore.GREEN}{image_url}{Fore.RESET} is downloaded")
         count += 1
     except HTTPError:
-        print(f"{image_url} returned HTTP error.")
+        print(f"{Fore.RED}{image_url}{Fore.RESET} returned HTTP error.")
 
 def format_image_link(image_tag):
     image_tag = image_tag.a["href"]
@@ -45,5 +48,5 @@ def main(url, foldername):
 
 if __name__ == '__main__':
     main(URL, folder_name)
-    print('')
-    print(f"Downloaded {count} files.")
+    print("")
+    print(f"{Fore.BLUE}Downloaded {str(count)} files.{Fore.RESET}")
