dc55378ec389b910bd5229ee464843f29bab8bd8
Faridin Tzy <73052959+Famewix@users.noreply.github.com>
2022-12-29 13:57:29 +0600

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -4,7 +4,6 @@ import requests
 from bs4 import BeautifulSoup
 import os, sys
 from colorama import Fore, init
-import threading
 
 ###########################################
 # https://github.com/Famewix
