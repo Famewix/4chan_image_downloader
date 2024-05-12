5a3744cddf2f3b2f44bbf5767975ab17ab957864
Faridin Islam <faridin05saif@gmail.com>
2024-05-12 12:32:35 +0600

    Added argparse

--- scrapper.py
+++ scrapper.py
@@ -4,58 +4,76 @@ import requests
 from bs4 import BeautifulSoup
 import os, sys
 from colorama import Fore, init
+import argparse
 
 ###########################################
-# https://github.com/Famewix
-# https://twitter.com/fl4wlessrokk
+# https://github.com/xonsium/
 ###########################################
+
+# all arguments
+arg_parser = argparse.ArgumentParser(prog='Scrape 4chan thread images', usage='%(prog)s [options]')
+arg_parser.add_argument('-u', '--url', type=str, help='4chan thread url', required=True, metavar='')
+arg_parser.add_argument('-f', '--fname', type=str, help='folder name', required=True, metavar='')
+
+args = arg_parser.parse_args()
 
 init(autoreset=True)
 
-URL = input("4chan thread URL: ")
-folder_name = input("Folder to save images: ")
+URL = args.url
+folder_name = args.fname
 count = 1
 downloaded_files = 0
+total = 1
 
 
 def download_image(image_url,  name):
-	global count, downloaded_files
-	try:
-		urlretrieve(image_url, name)
-		print(f"{count}. {Fore.GREEN}{image_url}{Fore.RESET} is downloaded")
-		downloaded_files += 1
-	except HTTPError:
-		print(f"{Fore.RED}{image_url}{Fore.RESET} returned HTTP error.")
-	count += 1
+    global count, downloaded_files
+    try:
+        urlretrieve(image_url, name)
+        downloaded_files += 1
+        print(f"{count}. Downloaded {Fore.GREEN}{image_url}{Fore.RESET} -- {round((downloaded_files*100)/total, 2)}%")
+    except HTTPError:
+        print(f"{Fore.RED}{image_url}{Fore.RESET} returned HTTP error.")
+    count += 1
 
 def format_image_link(image_tag):
-	image_tag = image_tag.a["href"]
-	image_tag = f"https:{image_tag}"
-	return image_tag
+    image_tag = image_tag.a["href"]
+    image_tag = f"https:{image_tag}"
+    return image_tag
 
 
 def main(url, foldername):
-	response = requests.get(url)
-	if response.status_code != 200:
-		print(f"Status: {response.status_code} — Invalid URL I guess.\n")
-		sys.exit()
+    ### https://boards.4chan.org/hr/thread/4551968
+    global total, count
+    thread_id = url.split("/")[-1]
+    response = requests.get(url)
+    if response.status_code != 200:
+        print(f"Status: {response.status_code} — Invalid URL I guess.\n")
+        sys.exit()
 
-	bs = BeautifulSoup(response.content, 'html.parser')
-	div_with_image = bs.find_all("div", attrs={"class":"fileText"})
-	try:
-		os.mkdir(foldername)
-	except FileExistsError:
-		pass
-	os.chdir(foldername)
+    bs = BeautifulSoup(response.content, 'html.parser')
+    div_with_image = bs.find_all("div", attrs={"class":"fileText"})
+    try:
+        os.mkdir(f"{foldername}_{thread_id}")
+    except FileExistsError:
+        pass
+    os.chdir(f"{foldername}_{thread_id}")
+    files = os.listdir()
+    total = len(div_with_image)
+    print(f"{Fore.BLUE}Found {total} media files.{Fore.RESET}")
 
-	print(f"{Fore.BLUE}Found {len(div_with_image)} media files.{Fore.RESET}")
-
-	for tags in div_with_image:
-		image_url = format_image_link(tags)
-		name_ = image_url.split('/')[4]
-		download_image(image_url, name_)
+    for tags in div_with_image:
+        image_url = format_image_link(tags)
+        name_ = image_url.split('/')[4]
+        if name_ not in files:
+            download_image(image_url, name_)
+        else:
+            print(f"{count}.{Fore.GREEN} Skipping {image_url}.{Fore.RESET}")
+            total -= 1
+            count += 1
 
 if __name__ == '__main__':
-	main(URL, folder_name)
-	print('')
-	print(f"{Fore.BLUE}Downloaded {downloaded_files} files.{Fore.RESET}")
+    main(URL, folder_name)
+    print('')
+    print(f"{Fore.BLUE}Downloaded {downloaded_files} files.{Fore.RESET}")
+
